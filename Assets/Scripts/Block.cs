028784c0b799e2aea1455528a03c78d01186e8b6
riekure <s2darl0uvgia24@gmail.com>
2019-04-07 23:39:23 +0900

    process p.326

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // ①隙間の高さを調整
+    public float minHeight;
+    public float maxHeight;
+    // ②Pivotオブジェクト
+    public GameObject pivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 開始時に隙間の高さを変更
+        ChangeObject();  // ③隙間の初期化
+    }
+
+    void ChangeObject()
+    {
+        // ④高さの変更
+        // ランダムな高さを生成して設定
+        float height = Random.Range(minHeight, maxHeight);
+        pivot.transform.localPosition = new Vector3(0.0f, height, 0.0f);
+    }
+
+    // ScrollObjectスクリプトからのメッセージを受け取って高さを変更
+    void OnScrollEnd()
+    {
+        // ⑤スクロール完了時の隙間の再設定
+        ChangeObject();
+    }
+}
